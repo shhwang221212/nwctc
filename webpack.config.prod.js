61903f20ebdc3f6a386ecffbee63575d252d4046
shhwang1212 <shhwang1212@lguplus.co.kr>
2026-09-11 14:31:15 +0900

    nwctc

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -7,7 +7,7 @@ module.exports = merge(common, {
   mode: 'production',
   plugins: [
     new HtmlWebpackPlugin({
-      template: './index.html',
+      template: './nwctc.html',
     }),
     new CopyPlugin({
       patterns: [
